014b5acda48a62b41ac30b402280f5f0d18ff767
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-07-07 02:12:42 +0300

    coding 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
