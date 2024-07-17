ee4ab518919a4f06228ba98e466b23210b88e894
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-07-17 22:46:39 +0300

    Stack: push, pop

--- src/main/java/org/example/stack/Main.java
+++ src/main/java/org/example/stack/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Stack myStack = new Stack(4);
+        myStack.push(2);
+        myStack.push(5);
+        myStack.pop();
 
         myStack.printStack();
     }
-
 
 }
--- src/main/java/org/example/stack/Stack.java
+++ src/main/java/org/example/stack/Stack.java
@@ -28,4 +28,25 @@ public class Stack {
         }
     }
 
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (height == 0) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+
+    public Node pop() {
+        if (height == 0) return null;
+
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+
+        return temp;
+    }
 }
