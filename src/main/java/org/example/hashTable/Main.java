0e6fa2809e7267240b94bae92e8d3b1566abc2bc
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-08-09 23:38:59 +0300

    implement set, hash methode

--- src/main/java/org/example/hashTable/Main.java
+++ src/main/java/org/example/hashTable/Main.java
@@ -3,6 +3,11 @@ package org.example.hashTable;
 public class Main {
     public static void main(String[] args) {
         HashTable myhashTable = new HashTable();
+
+        myhashTable.set("kazz", 6);
+        myhashTable.set("Hash table", 9);
+        myhashTable.set("henimbola", 10);
+
         myhashTable.printTable();
     }
 }
